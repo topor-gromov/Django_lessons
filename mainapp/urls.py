4494a1b7161288114fe282555193c700117c5e51
topor-gromov <92143832+topor-gromov@users.noreply.github.com>
2022-09-24 20:04:22 +0500

    Практическое задание №3

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
-from .views import *
+from mainapp import views
+from mainapp.apps import MainappConfig
+
+app_name = MainappConfig.name
 
 urlpatterns = [
-    path('', MainPageView.as_view()),
-    path('login/', LoginPageView.as_view()),
-    path('doc_site/', DocSitePageView.as_view()),
-    path('contacts/', ContactsPageView.as_view()),
-    path('news/', NewsPageView.as_view()),
-    path('courses_list/', CoursesListPageView.as_view())
+    path("", views.MainPageView.as_view(), name="main_page"),
+    path("news/", views.NewsPageView.as_view(), name="news"),
+    path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
+    path("courses/", views.CoursesPageView.as_view(), name="courses"),
+    path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
+    path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
+    path("login/", views.LoginPageView.as_view(), name="login"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,20 +1,44 @@
+from datetime import datetime
+
 from django.views.generic import TemplateView
 
+
 class MainPageView(TemplateView):
-    template_name = 'mainapp\index.html'
+    template_name = "mainapp/index.html"
 
-class ContactsPageView(TemplateView):
-    template_name = 'mainapp\contacts.html'
-
-class LoginPageView(TemplateView):
-    template_name = 'mainapp\login.html'
 
 class NewsPageView(TemplateView):
-    template_name = "mainapp\\news.html"
+    template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+        # Get all previous data
+        context = super().get_context_data(**kwargs)
+        # Create your own data
+        context["news_title"] = "Громкий новостной заголовок"
+        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        context["range"] = range(5)
+        context["datetime_obj"] = datetime.now()
+        return context
+
+
+class NewsWithPaginatorView(NewsPageView):
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(page=page, **kwargs)
+        context["page_num"] = page
+        return context
+
+
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/courses_list.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
 
 class DocSitePageView(TemplateView):
-    template_name = 'mainapp\doc_site.html'
+    template_name = "mainapp/doc_site.html"
 
-class CoursesListPageView(TemplateView):
-    template_name = 'mainapp\courses_list.html'
 
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
